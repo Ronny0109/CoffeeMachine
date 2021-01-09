bbfd935c0d9d934a211081934b9263d0458897f5
ronnyfalke <ronnyfalke.telekom@gmail.com>
2021-01-09 16:05:02 +0100

    der start

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,27 @@
+package machine;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+   /* int milk  = 540;
+    int coffeeBeans = 120;
+    int disposableCups = 9;
+    int money = 550;
+
+    enum mode {
+        CHOOSINGANACTION, CHOOSINGAVARIANT
+    }
+
+*/
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+
+
+/*
+i
+ */
